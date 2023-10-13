b0ee076e8cb88acd0c68e04c8daed7efdc84fc51
NimishaNK <nimishankanabar@gmail.com>
2023-10-13 17:12:18 +0530

    task completed

--- SauceDemoProject/src/main/java/pageLayer/LoginPage.java
+++ SauceDemoProject/src/main/java/pageLayer/LoginPage.java
@@ -1,5 +1,37 @@
 package pageLayer;
 
-public class LoginPage {
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
 
-}
+	public class LoginPage {
+		WebDriver driver;
+		public LoginPage (WebDriver driver) {
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+		}
+		@FindBy(xpath = "//input[@id='user-name']")
+		private WebElement username_txtbox;
+		@FindBy(xpath = "//input[@id='password']")
+		private WebElement password_txtbox;
+		@FindBy(xpath = "//input[@id='login-button']")
+		private WebElement login_btn;
+		
+		public void enterusername(String username) {
+			username_txtbox.sendKeys(username);
+		}
+		public void enterpassword(String password) {
+			password_txtbox.sendKeys(password);
+		}
+		public void clickonloginbtn() {
+			login_btn.click();
+		}
+		public String getCurl() {
+			String url = driver.getCurrentUrl();
+			return url;
+		}
+		
+
+	}
+
--- SauceDemoProject/src/main/java/testBase/TestBasePage.java
+++ SauceDemoProject/src/main/java/testBase/TestBasePage.java
@@ -1,5 +1,39 @@
 package testBase;
 
-public class TestBasePage {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBasePage {
+	public static WebDriver driver;
+	@BeforeMethod
+	public void setUp() {
+		String br= "Chrome";
+		if(br.equalsIgnoreCase("chrome")) {
+			driver= new ChromeDriver();
+		}
+		else if(br.equalsIgnoreCase("Firefox")) {
+			driver= new FirefoxDriver();
+		}
+		else if(br.equalsIgnoreCase("Edge")) {
+			driver= new EdgeDriver();
+		}
+		else{
+			System.out.println("Provid valid browser name");
+		}
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));		
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+		
+	}
 }
